6d5dfa68293a261b43ba7e74788dbd9c98b13d28
Muhammed Salman <salman8official@gmail.com>
2024-12-31 21:39:40 +0530

    Update Cart.jsx

--- frontend/src/screens/Cart/Cart.jsx
+++ frontend/src/screens/Cart/Cart.jsx
@@ -48,12 +48,12 @@ const Cart = () => {
                 <hr/>
                 <div className="cart-total-details">
                     <p>Delivery Fee</p>
-                    <p>₹{getTotalCartAmount()==0?0:20}</p>
+                    <p>₹{getTotalCartAmount()==0?0: 20}</p>
                 </div>
                 <hr/>
                 <div className="cart-total-details">
                     <b>Total</b>
-                    <b>₹{getTotalCartAmount()==0?0:getTotalCartAmount() + 20}</b>
+                    <b>₹{getTotalCartAmount()==0?0:getTotalCartAmount() +20}</b>
                 </div>
             </div>
             
