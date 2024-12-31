4ccb172188144b288574148ccc9bed57d37422f2
Muhammed Salman <salman8official@gmail.com>
2024-12-31 21:21:16 +0530

    Update PlaceOrder.jsx

--- frontend/src/screens/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/screens/PlaceOrder/PlaceOrder.jsx
@@ -92,12 +92,12 @@ const PlaceOrder = () => {
                 <hr/>
                 <div className="cart-total-details">
                     <p>Delivery Fee</p>
-                    <p>₹{getTotalCartAmount()==0?0:2}</p>
+                    <p>₹{getTotalCartAmount()==0?0:20}</p>
                 </div>
                 <hr/>
                 <div className="cart-total-details">
                     <b>Total</b>
-                    <b>₹{getTotalCartAmount()==0?0:getTotalCartAmount() + 2}</b>
+                    <b>₹{getTotalCartAmount()==0?0:getTotalCartAmount() + 20}</b>
                 </div>
           </div>
 
