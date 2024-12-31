0f9d72617053831daa31a336a848d20812c5b3b3
Muhammed Salman <salman8official@gmail.com>
2024-12-31 20:43:53 +0530

    ud

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,8 +5,8 @@ const Stripe = require('stripe')
 const stripe = new Stripe(process.env.SK_SECRET)
 
 const placeOrder = async (req, res) => {
-    const frontend_url = "https://food-del-frontend-fp7s.onrender.com"
-    // const frontend_url = "http://localhost:5173"
+    // const frontend_url = "https://food-del-frontend-fp7s.onrender.com"
+    const frontend_url = "http://localhost:5173"
     try {
         const newOrder = await orderModel.create({
             userId: req.body.userId,//
