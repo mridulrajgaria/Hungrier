b5e00493229b895fd33558f472d3f12886502e0c
muhamedthahir <thahirtf17@gmail.com>
2025-02-03 11:53:52 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   const [showLogin, setShowLogin] = React.useState(false);
+  
   return (
     <>
       {showLogin && <LoginPopup setShowLogin={setShowLogin} />}
