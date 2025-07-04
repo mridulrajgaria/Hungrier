e68d12f06c699237317d2d843ff9ba79c9625178
mridulrajgaria <iammridul222@gmail.com>
2025-07-04 22:59:29 +0530

    Delete backend directory

--- backend/routes/userRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const userRouter = express.Router();
-const {loginUser,registerUser} = require('../controllers/userController')
-
-userRouter.post('/register',registerUser)
-userRouter.post('/login',loginUser)
-
-module.exports = userRouter;
